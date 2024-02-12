e59a38417faaec2fbf6272299fbf4d0e2cda774e
Hideaki223 <lucas.godard@proton.me>
2024-02-12 17:44:49 +0100

    Création de la structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {SideMenuComponent} from "./side-menu/side-menu.component";
 import {FormMessageComponent} from "./form-message/form-message.component";
+import {HeaderComponent} from "./header/header.component";
+import {ListeMessageComponent} from "./liste-message/liste-message.component";
+import {BlogDescriptionComponent} from "./blog-description/blog-description.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SideMenuComponent, FormMessageComponent],
+  imports: [RouterOutlet, SideMenuComponent, FormMessageComponent, HeaderComponent, ListeMessageComponent, BlogDescriptionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
