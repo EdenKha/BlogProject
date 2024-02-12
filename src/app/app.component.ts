d02288e8a642368c4e48a3175365b78184fcc3e1
AzureAD\HuguesAnsoborlo <hugues.ansoborlo@toulouse.miage.fr>
2024-02-12 15:31:51 +0100

    Test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {SideMenuComponent} from "./side-menu/side-menu.component";
+import {FormMessageComponent} from "./form-message/form-message.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SideMenuComponent, FormMessageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
