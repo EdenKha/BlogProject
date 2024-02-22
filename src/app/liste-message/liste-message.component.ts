83f226f45feea3a9b37c8ad47844927371f8489e
Hideaki223 <lucas.godard@proton.me>
2024-02-22 17:08:30 +0100

    on avance tranquillement sur le front

--- src/app/liste-message/liste-message.component.ts
+++ src/app/liste-message/liste-message.component.ts
@@ -1,12 +1,14 @@
  import { Component } from '@angular/core';
  import {RouterLink} from "@angular/router";
+ import {MessageComponent} from "../message/message.component";
 
 
 @Component({
   selector: 'app-liste-message',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    MessageComponent
   ],
   templateUrl: './liste-message.component.html',
   styleUrl: './liste-message.component.css'
