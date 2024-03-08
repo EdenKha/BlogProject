8c56ed3bdd94a9929c472ab64d83ebbc61ff8ffb
Hideaki223 <lucas.godard@proton.me>
2024-03-08 10:03:08 +0100

    test angular

---
+++ angular/src/app/liste-message/liste-message.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
+import {MessageComponent} from "../message/message.component";
+import {FormMessageComponent} from "../form-message/form-message.component";
+import {NgForOf} from "@angular/common";
+import {MessageService} from "../message.service";
+
+
+@Component({
+  selector: 'app-liste-message',
+  standalone: true,
+  imports: [
+    RouterLink,
+    MessageComponent,
+    FormMessageComponent,
+    NgForOf
+  ],
+  templateUrl: './liste-message.component.html',
+  styleUrl: './liste-message.component.css'
+})
+export class ListeMessageComponent {
+  messages: any[] = [];
+
+  constructor(private messageService: MessageService) { }
+
+  ngOnInit() {
+    this.messageService.currentMessages.subscribe(messages => this.messages = messages);
+  }
+}
---
+++ angular/src/app/message/message.component.ts
@@ -0,0 +1,22 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {NgForOf} from "@angular/common";
+import {MessageService} from "../message.service";
+
+@Component({
+  selector: 'app-message',
+  standalone: true,
+  imports: [
+    NgForOf
+  ],
+  templateUrl: './message.component.html',
+  styleUrl: './message.component.css'
+})
+export class MessageComponent implements OnInit{
+  @Input() message: any;
+
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+}
