faacf0a6d72053499ebecfdfa39cabc11d8bd7bb
AzureAD\HuguesAnsoborlo <zorhugues@hotmail.fr>
2024-03-13 11:14:51 +0100

    commit connexion back et front

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,18 +1,38 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {SideMenuComponent} from "./side-menu/side-menu.component";
 import {FormMessageComponent} from "./form-message/form-message.component";
 import {HeaderComponent} from "./header/header.component";
 import {ListeMessageComponent} from "./liste-message/liste-message.component";
 import {BlogDescriptionComponent} from "./blog-description/blog-description.component";
+import {ApiService} from "./api.service";
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, SideMenuComponent, FormMessageComponent, HeaderComponent, ListeMessageComponent, BlogDescriptionComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  standalone: true,
+  imports: [
+    FormMessageComponent,
+    BlogDescriptionComponent,
+    ListeMessageComponent,
+    SideMenuComponent,
+    HeaderComponent
+  ],
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ProjetBlog';
+export class AppComponent implements OnInit {
+  title = 'frontEnd';
+  message: any;
+  constructor(private apiService: ApiService) { };
+  ngOnInit() {
+    this.apiService.getMessage().subscribe(data => {
+      this.message = data;
+    });
+  }
+
+  onClick() {
+    this.apiService.sendMessage({title: 'Hello World'}).subscribe(data => {
+      this.message = data;
+    });
+  }
 }
